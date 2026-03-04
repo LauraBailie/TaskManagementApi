4b9e3818355579acdc3e1b93755a3dc88ca07ef4
Laura Bailie <laurabailie11@gmail.com>
2026-03-04 09:04:14 +0200

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace TaskManagementApi.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void PassingTest()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void FailingTest_ForDemo()
+    {
+        Assert.Equal(2, 1 + 1);   // will pass
+    }
+}
